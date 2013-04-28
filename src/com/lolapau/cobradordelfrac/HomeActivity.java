d1704ed72e5d4a8dc4f6fd4d966b16cf55ac18f7
Lola Priego <lola@emtrics.com>
2013-04-28 13:26:05 +0200

    Added Home Activity class

---
+++ src/com/lolapau/cobradordelfrac/HomeActivity.java
@@ -0,0 +1,26 @@
+package com.lolapau.cobradordelfrac;
+
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+import com.example.cobradordelfrac.R;
+
+public class HomeActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_home);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.home, menu);
+		return true;
+	}
+
+}
